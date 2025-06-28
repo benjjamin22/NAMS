0ca8ae6d928dbcd880193ac2a860e947501b8852
benjjamin22 <122483173+benjjamin22@users.noreply.github.com>
2025-06-28 08:52:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ async function uploadImageToGoogleDrive(file) {
 
 app.get('/detail', async(req, res) => {
     try {
-        const data = await Note.find() .sort({_id:-1}'-PhoneNo -EmergencyNo');
+        const data = await Note.find() .sort({_id:-1},'-PhoneNo -EmergencyNo');
         res.json(data);
     } catch (err) {
         console.log(err);
