b97d8027938499f0f98e88dddd39cb30dbf8eae7
benjjamin22 <122483173+benjjamin22@users.noreply.github.com>
2025-08-08 17:53:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -362,7 +362,7 @@ app.get('/new', (req, res) => {
   });
      
 //UPDATE ROUT
-app.post('/editit/:id', async(req, res) => {
+app.post('/editit/:_id', async(req, res) => {
 const {id} = req.params;
 try{
   const founduser = await Note.findById(id);
